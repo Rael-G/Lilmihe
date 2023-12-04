9524df5eb210fc491507f97be2a7da274e95236b
Israel Albino Galvan <israel.galvan@live.com>
2023-12-04 16:28:10 -0300

    Delagated the handling errors responsability to clients

--- DbUpInBox.Core/DbMigrator.cs
+++ DbUpInBox.Core/DbMigrator.cs
@@ -1,4 +1,3 @@
-using System.Net.Sockets;
 using Microsoft.Extensions.Logging;
 using DbUp.Engine;
 
@@ -7,90 +6,32 @@ namespace DbUpInBox;
 public abstract class DbMigrator(ILogger logger, string connectionString)
 {
     public string ConnectionString { get; set; } = connectionString;
-    private readonly ILogger _logger = logger;
+    public ILogger _logger { get; set; } = logger;
+
+    private const string MIGRATION_SUCCESS = "Migration Success.";
+    private const string SEED_SUCCESS = "Seed Success.";
 
     public void Migrate(string scriptsPath)
     {
-        if (!Directory.Exists(scriptsPath))
-        {
-            _logger.LogError($"{Messages.MIGRATION_ERROR} Directory not found.",  new[] { scriptsPath });
-            return;
-        }
-
-        DatabaseUpgradeResult result;
-
-        try 
-        {
-            result = ExecuteMigration(ConnectionString, scriptsPath);
-        }
-        catch (SocketException ex)
-        {
-            _logger.LogError(ex, $"{Messages.MIGRATION_ERROR} {Messages.SOCKET_ERROR}",  new[] {ConnectionString});
-            return;
-        }
-        catch (ArgumentException ex)
-        {
-            _logger.LogError(ex, $"{Messages.MIGRATION_ERROR} {Messages.CONNECTION_STRING_ERROR}",  new[] {ConnectionString});
-            return;
-        }
-        catch(DbConnectionException ex)
-        {
-            _logger.LogError(ex, $"{Messages.MIGRATION_ERROR} {Messages.CONNECTION_STRING_ERROR}", new[] {ConnectionString});
-            return;
-        }
-        catch(Exception ex)
-        {
-            _logger.LogError(ex, Messages.MIGRATION_ERROR);
-            return;
-        }
+        var result = ExecuteMigration(ConnectionString, scriptsPath);
 
         if (result.Successful)
-            _logger.LogInformation(Messages.MIGRATION_SUCCESS);
+            _logger.LogInformation(MIGRATION_SUCCESS);
         else
-            _logger.LogError(Messages.MIGRATION_ERROR, new[] {result});
+            throw new DbException($"DB exception has occurred in script '{result.ErrorScript.Name}'\n{result.Error}");
     }
 
     public void Seed(string seedPath)
     {
-        if (!Directory.Exists(seedPath))
-        {
-            _logger.LogError($"{Messages.SEED_ERROR} {Messages.DIRECTORY_ERROR}", new[] { seedPath });
-            return;
-        }
-
-        DatabaseUpgradeResult result;
-        try 
-        {
-            result = ExecuteMigration(ConnectionString, seedPath);
-        }
-        catch (SocketException ex)
-        {
-            _logger.LogError(ex, $"{Messages.SEED_ERROR} {Messages.SOCKET_ERROR}", new[] {ConnectionString});
-            return;
-        }
-        catch (ArgumentException ex)
-        {
-            _logger.LogError(ex, $"{Messages.SEED_ERROR} {Messages.CONNECTION_STRING_ERROR}", new[] {ConnectionString});
-            return;
-        }
-        catch(DbConnectionException ex)
-        {
-            _logger.LogError(ex, $"{Messages.SEED_ERROR} {Messages.CONNECTION_STRING_ERROR}", new[] {ConnectionString});
-            return;
-        }
-        catch(Exception ex)
-        {
-            _logger.LogError(ex, Messages.SEED_ERROR);
-            return;
-        }
+        var result = ExecuteMigration(ConnectionString, seedPath);
 
         if (result.Successful)
-            _logger.LogInformation(Messages.SEED_SUCCESS);
+            _logger.LogInformation(SEED_SUCCESS);
         else
-            _logger.LogError(Messages.SEED_ERROR, new[] {result});
+            throw new DbException($"DB exception has occurred in script '{result.ErrorScript.Name}'\n{result.Error}");
+
     }
 
     protected abstract DatabaseUpgradeResult ExecuteMigration
         (string connectionString, string scriptsPath);
-
 }
---
+++ DbUpInBox.Core/Exceptions/DbException.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+
+namespace DbUpInBox;
+
+[DebuggerDisplay($"{{{nameof(DebuggerDisplay)}(),nq}}")]
+public class DbException : System.Data.Common.DbException
+{
+    public const string DB_ERROR = "Db Error.";
+    public DbException() 
+        : base(DB_ERROR) {}
+    public DbException(string msg) 
+        : base($"{DB_ERROR} {msg}") {}
+
+    public DbException(string msg, Exception inner) 
+        : base($"{DB_ERROR} {msg}", inner) {}
+
+    private string DebuggerDisplay => ToString();
+}
