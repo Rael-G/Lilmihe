02f6515df41971b86a64b5f588068a38ca345d92
Israel Albino Galvan <israel.galvan@live.com>
2023-12-04 16:43:15 -0300

    Interactivity and error handling

---
+++ DbUpInBox.Cli/Messages.cs
@@ -0,0 +1,20 @@
+﻿namespace DbUpInBox;
+
+public static class Messages
+{
+    public const string MIGRATION_ERROR = "Migration Failed.";
+    public const string CONNECTION_ERROR = "Connection Error.";
+    public const string CONNECTION_STRING_ERROR = "Invalid Connection String.";
+    public const string SEED_ERROR = "Seed Failed.";
+    public const string DIRECTORY_ERROR = "Directory not found.";
+    public const string TITLE = @"
+  _____  _     _    _      _____       ____            
+ |  __ \| |   | |  | |    |_   _|     |  _ \           
+ | |  | | |__ | |  | |_ __  | |  _ __ | |_) | _____  __
+ | |  | | '_ \| |  | | '_ \ | | | '_ \|  _ < / _ \ \/ /
+ | |__| | |_) | |__| | |_) || |_| | | | |_) | (_) >  < 
+ |_____/|_.__/ \____/| .__/_____|_| |_|____/ \___/_/\_\
+                     | |                               
+                     |_|                               
+";
+}
--- DbUpInBox.Cli/Program.cs
+++ DbUpInBox.Cli/Program.cs
@@ -1,34 +1,115 @@
-﻿using DbUpInBox;
+﻿using System.Net.Sockets;
+using DbUpInBox;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
-var connectionString = args.FirstOrDefault();
-var dbms = args.ElementAtOrDefault(1);
+Console.Clear();
+Console.WriteLine(Messages.TITLE);
+var connectionString = string.Empty;
+while(connectionString.IsNullOrEmpty())
+{
+    Console.WriteLine("Paste your Connection String:");
+    connectionString = Console.ReadLine()?? string.Empty;
+}
+Console.Clear();
+Console.WriteLine(Messages.TITLE);
+var dbms = string.Empty;
+while (dbms.IsNullOrEmpty())
+{
+    Console.WriteLine("Type your DBMS:");
+    Console.WriteLine("Options are <postgresql>");
+    dbms = Console.ReadLine()?? string.Empty;
+}
+
+Console.Clear();
+Console.WriteLine(Messages.TITLE);
 
 using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
 ILogger logger = factory.CreateLogger("DbUpInBox");
+factory.Dispose();
 
 DbMigrator migrator;
-
 try
 {
     migrator = MigratorInjector.Inject(connectionString, dbms, logger);
 }
-catch(InputException ex)
+catch(ArgumentException ex)
 {
-    logger.LogError(ex, ex.Message);
+    logger.LogError(ex, Messages.MIGRATION_ERROR);
     return -1;
 }
 
-var scriptsPath =  Path.Combine(".", "Scripts");
-migrator.Migrate(scriptsPath);
- 
-var isDevelopmentEnvironment = string.Equals(Environment.GetEnvironmentVariable
-    ("ENVIRONMENT"), "Development", StringComparison.OrdinalIgnoreCase);
 
-if (!isDevelopmentEnvironment)
+var scriptsPath = string.Empty;
+while (scriptsPath.IsNullOrEmpty())
+{
+    Console.WriteLine("Paste the path to scripts:");
+    scriptsPath = Console.ReadLine()?? string.Empty;
+}
+
+try
+{
+    migrator.Migrate(scriptsPath);
+}
+catch(DirectoryNotFoundException ex)
+{
+    logger.LogError(ex, Messages.DIRECTORY_ERROR, new[]{ scriptsPath });
+    return -1;
+}
+catch (SocketException ex)
+{
+    logger.LogError(ex, $"{Messages.MIGRATION_ERROR} {Messages.CONNECTION_ERROR}",  new[] {connectionString});
+    return -1;
+}
+catch (ArgumentException ex)
+{
+    logger.LogError(ex, $"{Messages.MIGRATION_ERROR} {Messages.CONNECTION_STRING_ERROR}",  new[] {connectionString});
+    return -1;
+}
+catch(DbException ex)
+{
+    logger.LogError(ex, $"{Messages.MIGRATION_ERROR} {Messages.CONNECTION_STRING_ERROR}", new[] {connectionString});
+    return -1;
+}
+
+Console.WriteLine("Do you want to run seeds? (Y/N)");
+var key = Console.ReadLine();
+if (key is null || !key.Equals("Y", StringComparison.CurrentCultureIgnoreCase))
     return 0;
 
-var seedPath = Path.Combine(scriptsPath, "Seeds");
-migrator.Seed(seedPath);
+Console.Clear();
+Console.WriteLine(Messages.TITLE);
+
+var seedPath = string.Empty;
+while (seedPath.IsNullOrEmpty())
+{
+    Console.WriteLine("Paste the path to seeds:");
+    seedPath = Console.ReadLine()?? string.Empty;
+}
+
+try
+{
+    migrator.Seed(seedPath);
+}
+catch(DirectoryNotFoundException ex)
+{
+    logger.LogError(ex, Messages.DIRECTORY_ERROR, new[]{ scriptsPath });
+    return -1;
+}
+catch (SocketException ex)
+{
+    logger.LogError(ex, $"{Messages.SEED_ERROR} {Messages.CONNECTION_ERROR}",  new[] {connectionString});
+    return -1;
+}
+catch (ArgumentException ex)
+{
+    logger.LogError(ex, $"{Messages.SEED_ERROR} {Messages.CONNECTION_STRING_ERROR}",  new[] {connectionString});
+    return -1;
+}
+catch(DbException ex)
+{
+    logger.LogError(ex, $"{Messages.SEED_ERROR}");
+    return -1;
+}
 
 return 0;
