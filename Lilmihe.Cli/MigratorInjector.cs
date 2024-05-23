f0ac577dede9c9d2936f8475f7913ba26361821a
Raelg-G <israel.galvan@live.com>
2024-05-22 23:04:41 -0300

    Refactor remove interactive mode and transform .Cli in a true console app

--- Lilmihe.Cli/MigratorInjector.cs
+++ Lilmihe.Cli/MigratorInjector.cs
@@ -4,11 +4,12 @@ namespace Lilmihe;
 
 public static class MigratorInjector
 {
-    public static MigrationHelper Inject(string dbms, string connectionString, string scriptsPath)
+    public const int Sqlite = 0;
+    public static MigrationHelper Inject(int dbms, string connectionString, string scriptsPath)
     { 
         return dbms switch
         {
-            "sqlite" => new MigrationHelper(scriptsPath, new SqliteConnection(connectionString)),
+            Sqlite => new MigrationHelper(scriptsPath, new SqliteConnection(connectionString)),
             _ => throw new ArgumentException
                 ($"{dbms} is an invalid DBMS option. Please provide a valid DataBase Management System name as an argument."),
         };
--- Lilmihe.Cli/Program.cs
+++ Lilmihe.Cli/Program.cs
@@ -1,86 +1,95 @@
-﻿using Lilmihe;
+﻿using System.CommandLine;
+using Lilmihe;
 using Microsoft.Extensions.Logging;
 
-Console.Clear();
-Console.WriteLine(Messages.TITLE);
-var dbms = string.Empty;
-while (string.IsNullOrEmpty(dbms))
+public partial class Program
 {
-    Console.WriteLine("Type your DBMS:");
-    Console.WriteLine("Options are <sqlite>");
-    dbms = Console.ReadLine()?? string.Empty;
-}
-
-Console.Clear();
-Console.WriteLine(Messages.TITLE);
-var connectionString = string.Empty;
-while(string.IsNullOrEmpty(connectionString))
-{
-    Console.WriteLine("Paste your Connection String:");
-    connectionString = Console.ReadLine()?? string.Empty;
-}
-
-Console.Clear();
-Console.WriteLine(Messages.TITLE);
-var scriptsPath = string.Empty;
-while (string.IsNullOrEmpty(scriptsPath))
-{
-    Console.WriteLine("Paste the path to scripts:");
-    scriptsPath = Console.ReadLine()?? string.Empty;
-}
-
-using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
-ILogger logger = factory.CreateLogger("DbUpInBox");
-factory.Dispose();
-
-MigrationHelper migrator;
-try
-{
-    migrator = MigratorInjector.Inject(dbms, connectionString, scriptsPath);
-}
-catch(ArgumentException ex)
-{
-    logger.LogError(ex, Messages.MIGRATION_ERROR);
-    return -1;
-}
-
-var result = await migrator.Migrate();
-
-var successMessage = string.Empty;
-var failMessage = string.Empty;
-if (result.Success || result.SuccessFiles.Length > 0)
-    successMessage += Messages.MIGRATION_SUCCESS + "\n";
-
-if (result.SuccessFiles.Length > 0)
-{
-    successMessage += "Scripts:\n";
-    foreach (var file in result.SuccessFiles)
+    public static async Task<int> Main(string[] args)
     {
-        successMessage += file + "\n";
+        var connectionString = new Option<string>(
+            ["--connection-string", "-c"],
+            "The connection string to the database");
+        var scriptsPath = new Option<string>(
+            ["--path", "-p"],
+            "The path to the SQL scripts");
+        var sqlite = new Option<bool>(
+            [ "--sqlite"] ,
+            "Specify SQLite as the DBMS");
+
+        var rootCommand = new RootCommand("Little database migration helper")
+        {
+            connectionString,
+            scriptsPath,
+            sqlite
+        };
+
+        rootCommand.SetHandler(Run, connectionString, scriptsPath, sqlite);
+
+        return await rootCommand.InvokeAsync(args);
     }
-    successMessage += "\n";
-}
-else if (result.Success)
-{
-    successMessage += result.Message +="\n";
-}
 
-if (!string.IsNullOrEmpty(successMessage))
-    logger.LogInformation(successMessage);
+    public static async Task<int> Run(string connectionString, string scriptsPath, bool sqlite)
+    {
+        var dbms = -1;
+        if (sqlite) dbms = MigratorInjector.Sqlite;
 
-if(!result.Success)
-{
-    failMessage +=  Messages.MIGRATION_ERROR + "\n";
-    failMessage += !string.IsNullOrEmpty(result.Message)? result.Message + "\n" : string.Empty;
-    failMessage += (result.FailedFile != null)? "Failed File:\n" + result.FailedFile : string.Empty;
-    failMessage += (result.FailedCommand != null)? "Failed Command:\n" + result.FailedCommand : string.Empty;
-    failMessage += (result.Error != null)? "Error:\n" + result.Error.Message : string.Empty;
+        if ( dbms < 0 || string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(scriptsPath))
+        {
+            Console.WriteLine("Missing required arguments. Please provide a DBMS option, --connection-string, and --path.");
+            return 0;
+        }
+
+        var migrator = MigratorInjector.Inject(dbms, connectionString, scriptsPath);
+        return await RunMigration(migrator);
+    }
+
+    public static async Task<int> RunMigration(MigrationHelper migrator)
+    {
+        Console.WriteLine(Messages.TITLE);
+
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = factory.CreateLogger("Lilmihe");
+        factory.Dispose();
+
+        var result = await migrator.Migrate();
+
+        var successMessage = string.Empty;
+        var failMessage = string.Empty;
+        if (result.Success || result.SuccessFiles.Length > 0)
+            successMessage += Messages.MIGRATION_SUCCESS + "\n";
+
+        if (result.SuccessFiles.Length > 0)
+        {
+            successMessage += "Scripts:\n";
+            foreach (var file in result.SuccessFiles)
+            {
+                successMessage += file + "\n";
+            }
+            successMessage += "\n";
+        }
+        else if (result.Success)
+        {
+            successMessage += result.Message +="\n";
+        }
+
+        if (!string.IsNullOrEmpty(successMessage))
+            logger.LogInformation(successMessage);
+
+        if(!result.Success)
+        {
+            failMessage +=  Messages.MIGRATION_ERROR + "\n";
+            failMessage += !string.IsNullOrEmpty(result.Message)? result.Message + "\n" : string.Empty;
+            failMessage += (result.FailedFile != null)? "Failed File:\n" + result.FailedFile : string.Empty;
+            failMessage += (result.FailedCommand != null)? "Failed Command:\n" + result.FailedCommand : string.Empty;
+            failMessage += (result.Error != null)? "Error:\n" + result.Error.Message : string.Empty;
+        }
+
+        if (!string.IsNullOrEmpty(failMessage))
+        {
+            logger.LogError(failMessage);
+            return -1;
+        }
+
+        return 0;
+    }
 }
-
-if (!string.IsNullOrEmpty(failMessage))
-{
-    logger.LogError(failMessage);
-    return -1;
-}
-
-return 0;
